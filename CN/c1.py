e935dc639ccc8f857e41bb7c147d3eecf0df4c8e
lucascosta0509 <lucascosta2305@gmail.com>
2026-04-03 00:43:41 -0300

    primeiras alterações

--- CN/c1.py
+++ CN/c1.py
@@ -1,19 +1,26 @@
+import math
 print("INFORME")
 #primeira unidade
-teste1 = (floating(input("nota do primeiro teste: ")))
+teste1 = (float(input("nota do primeiro teste: ")))
 prova1 = (float(input("nota da primeira prova: ")))
 nna1 = (float(input("nota da primeira nna: ")))
-media1 = (teste1+prova1+nna1)/3
-restante = 28 - media1
-print("Sua primeira média foi de",media1,"ainda faltam",restante,"pontos" )
+if (teste1 or prova1 or nna1) >10 or (teste1 or prova1 or nna1) <0:
+    print("insira nota válida")
+else:
+    media1 = (teste1+prova1+nna1)/3
+    restante = 28 - media1
+    print(f"sua primeira média foi de {media1:.1f}, ainda faltam {restante:.1f} pontos")
 
 #segunda unidade
 teste2 = (float(input("nota do segundo teste: ")))
 prova2 = (float(input("nota da segunda prova: ")))
 nna2 = (float(input("nota da segunda nna: ")))
-media2 = (teste2+prova2+nna2)/3
-restante = 28 - (media1+media2)
-print("Sua segunda média foi de",media2,"ainda faltam",restante,"pontos" )
+if (teste2 or prova2 or nna2) >10 or (teste2 or prova2 or nna2) <0:
+    print("insira nota válida")
+else:
+    media2 = (teste2+prova2+nna2)/3
+    restante = 28 - (media1+media2)
+    print(f"sua segunda média foi de {media2:.1f}, ainda faltam {restante:.1f} pontos")
 
 #terceira unidade
 teste3 = (float(input("nota do terceiro teste: ")))
@@ -21,12 +28,16 @@ prova3 = (float(input("nota da terceira prova: ")))
 nna3 = (float(input("nota da terceira nna: ")))
 media3 = (teste3+prova3+nna3)/3
 restante = 28 - (media1+media2+media3)
-if restante > 0:
-    print("Sua terceira média foi de",media1,"ainda faltam",restante,"pontos" )
-elif restante < 0:
-    print("Você está passado com",-restante,"pontos de folga")
+
+if (teste3 or prova3 or nna3) >10 or (teste3 or prova3 or nna3) <0:
+    print("insira nota válida")
 else:
-    print ("você está passado")
+ if restante > 0:
+     print(f"sua terceira média foi de {media3:.1f}, ainda faltam {restante:.1f} pontos")
+ elif restante < 0:
+     print(f"você está passado com {-restante:.1f} pontos de folga")
+ else:
+     print ("você está passado")
 
 #quarta unidade
 teste4 = (float(input("nota do quarto teste: ")))
@@ -34,18 +45,21 @@ prova4 = (float(input("nota da quarta prova: ")))
 nna4 = (float(input("nota da quarta nna: ")))
 media4 = (teste4+prova4+nna4)/3
 restante = 28 - (media1+media2+media3+media4)
-if restante > 0:
-    print("Sua quarta média foi de",media4,"ainda faltam",restante,"pontos" )
-elif restante < 0:
-    print("Você está passado com",-restante,"pontos de folga")
+if (teste4 or prova4 or nna4) >10 or (teste4 or prova4 or nna4) <0:
+    print("insira nota válida")
 else:
-    print ("você está passado")
+ if restante > 0:
+     print(f"sua quarta média foi de {media4:.1f}, ainda faltam {restante:.1f} pontos")
+ elif restante < 0:
+     print(f"você está passado com {-restante:.1f} pontos de folga")
+ else:
+     print ("você está passado")
 
 #final
 mediafinal = (media1+media2+media3+media4)/4
 if mediafinal >= 7:
-    print ("Você passou com uma média final de",mediafinal)
+    print (f"você passou com uma média final de {mediafinal:.1f} pontos")
 elif mediafinal < 7:
-    print ("sua nota final foi de",mediafinal,"você está de recupéração")
+    print (f"sua nota final foi de {mediafinal:.1f}, você está de recuperação")
 else:
     print ("sua nota final não pode ser calculada")
