94814ed0819b652dcf61103094b3a6c1a6a1ff98
lucascosta0509 <lucascosta2305@gmail.com>
2026-05-04 22:32:06 -0300

    Total reformulação

--- CN/c1.py
+++ CN/c1.py
@@ -5,15 +5,15 @@ def calculadora(Unidade):
     while func == True:
      try:
             dados = []
-            teste =(float(input(f"\nNota da {Unidade}º teste: ")))
+            teste =(float(input(f"\nNota do {Unidade}º teste: ")))
             dados.append(teste)
             prova =(float(input(f"Nota da {Unidade}º prova: ")))
             dados.append(prova)
             naa =(float(input(f"Nota da {Unidade}º naa: ")))
             dados.append(naa)
             if (teste or prova or naa) >10 or (teste or prova or naa) <0:
-                print("insira nota válida, reinicie o programa")
-                break
+                print("insira uma nota válida")
+                continue
             else:
                 mediaU = sum(dados)/len(dados)
                 if mediaU % 1 >0.5:
@@ -25,4 +25,23 @@ def calculadora(Unidade):
                 return mediaU
      except ValueError:
          print ("Insira apenas números")
-         break
+        
+medias = []
+
+for i in range(1,5):
+    mediaU= calculadora(i)
+    medias.append(mediaU)
+    restante = 28 - sum(medias)
+    if restante > 0:
+        print(f"Sua média foi de {mediaU} pontos, ainda lhe faltam {restante} pontos")
+    elif restante < 0:
+        print(f"Você está passado com {-restante} pontos de folga")
+    else:
+        print("você está passado")
+mediaF = sum(medias)/len(medias)
+if mediaF >= 7:
+    print(f"\nSua nota final foi de {mediaF} pontos, você está passado")
+elif mediaF<7:
+    print(f"\nSua nota final foi de {mediaF} pontos, você está de recuperação")
+        
+        
