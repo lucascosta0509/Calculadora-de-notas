f34c24136a52b0adb588be7ddbe57f9e57e0a913
lucascosta0509 <lucascosta2305@gmail.com>
2026-05-12 21:04:46 -0300

    Atualização na calculadora alternativa

--- CN/C1 Alt.py
+++ CN/C1 Alt.py
@@ -1,22 +1,30 @@
-dados = []
+dados = {'nomes': [], 'notas': [],"status" : []}
 func = True
 def pegar_dados(nA):
     while func == True:
-        nome = input("digite seu nome ")
-        dados.append(nome)
-        m1 = float(input("digite sua primeira média: "))
-        m2 = float(input("digite sua segunda média: "))
-        m3 = float(input("digite sua terceira média: "))
-        m4 = float(input("digite sua quarta média: "))
-        mR = (m1 + m2 + m3 + m4 )/4
-        print (mR)
-
-        
-        
-        
-        
-        return dados
-nA = int(input("digite o número de alunos: "))
-for i in range(1, nA+1):
+        try:
+            nome = str(input("digite o nome do aluno: "))
+            dados["nomes"].append(nome)
+            m1 = float(input("digite a primeira média: "))
+            m2 = float(input("digite a segunda média: "))
+            m3 = float(input("digite a terceira média: "))
+            m4 = float(input("digite a quarta média: "))
+            ##adicionar sistema para bloquear números invalidos
+            mR = (m1 + m2 + m3 + m4 )/4
+            dados["notas"].append(mR)
+            if mR >= 7:
+                 estado = ("aprovado")
+            else:
+                 estado = ("reprovado")
+            dados["status"].append(estado)
+            return dados        
+        except ValueError:
+            print("Digite valores válidos")
+try:
+        nA = int(input("digite o número de alunos: "))
+except ValueError:
+     print("digite apenas números inteiros")        
+for i in range(1,nA+1):
     pegar_dados(i)
-print (dados)
+
+
